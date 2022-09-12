b7595cd29a0cc1d0ac7893ea3a682ed5678051e1
Hugo Peek <hugo@fractal-farming.com>
2022-09-12 10:07:07 +0800

    Add constructor to base class
    This should ensure the package is always loaded through the service class.

--- core/components/htmlpagedom/model/htmlpagedom/htmlpagedom.class.php
+++ core/components/htmlpagedom/model/htmlpagedom/htmlpagedom.class.php
@@ -11,4 +11,86 @@ require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
  */
 class HtmlPageDom
 {
+    /**
+     * A reference to the modX instance
+     * @var modX $modx
+     */
+    public modX $modx;
+
+    /**
+     * The namespace
+     * @var string $namespace
+     */
+    public $namespace = 'htmlpagedom';
+
+    /**
+     * The class options
+     * @var array $options
+     */
+    public $options = [];
+
+    /**
+     * HtmlPageDom constructor
+     *
+     * @param modX $modx A reference to the modX instance.
+     * @param array $options An array of configuration options. Optional.
+     */
+    function __construct(modX &$modx, array $options = [])
+    {
+        $this->modx =& $modx;
+        $this->namespace = $this->getOption('namespace', $options, $this->namespace);
+
+        $corePath = $this->modx->getOption($this->namespace. '.core_path', $options, $this->modx->getOption('core_path', null, MODX_CORE_PATH) . 'components/' . $this->namespace . '/');
+        $assetsPath = $this->modx->getOption($this->namespace . '.assets_path', $options, $this->modx->getOption('assets_path', null, MODX_ASSETS_PATH) . 'components/' . $this->namespace . '/');
+        $assetsUrl = $this->modx->getOption($this->namespace . '.assets_url', $options, $this->modx->getOption('assets_url', null, MODX_ASSETS_URL) . 'components/' . $this->namespace . '/');
+
+        $this->options = array_merge([
+            'namespace' => $this->namespace,
+            'corePath' => $corePath,
+            'modelPath' => $corePath . 'model/',
+            'vendorPath' => $corePath . 'vendor/',
+            'chunksPath' => $corePath . 'elements/chunks/',
+            'pagesPath' => $corePath . 'elements/pages/',
+            'snippetsPath' => $corePath . 'elements/snippets/',
+            'pluginsPath' => $corePath . 'elements/plugins/',
+            'controllersPath' => $corePath . 'controllers/',
+            'processorsPath' => $corePath . 'processors/',
+            'templatesPath' => $corePath . 'templates/',
+            'cachePath' => $this->modx->getOption('core_path') . 'cache/',
+            'assetsPath' => $assetsPath,
+            'assetsUrl' => $assetsUrl,
+            'jsUrl' => $assetsUrl . 'js/',
+            'cssUrl' => $assetsUrl . 'css/',
+            'imagesUrl' => $assetsUrl . 'img/',
+            'connectorUrl' => $assetsUrl . 'connector.php'
+        ], $options);
+
+        $this->modx->addPackage($this->namespace, $this->getOption('modelPath'));
+        $lexicon = $this->modx->getService('lexicon', 'modLexicon');
+        $lexicon->load($this->namespace . ':default');
+    }
+
+    /**
+     * Get a local configuration option or a namespaced system setting by key.
+     *
+     * @param string $key The option key to search for.
+     * @param array $options An array of options that override local options.
+     * @param mixed $default The default value returned if the option is not found locally or as a
+     * namespaced system setting; by default this value is null.
+     * @return mixed The option value or the default value specified.
+     */
+    public function getOption($key, $options = [], $default = null)
+    {
+        $option = $default;
+        if (!empty($key) && is_string($key)) {
+            if ($options != null && array_key_exists($key, $options)) {
+                $option = $options[$key];
+            } elseif (array_key_exists($key, $this->options)) {
+                $option = $this->options[$key];
+            } elseif (array_key_exists("$this->namespace.$key", $this->modx->config)) {
+                $option = $this->modx->getOption("$this->namespace.$key");
+            }
+        }
+        return $option;
+    }
 }
